73d25806a37fa5ccb8b8aeec849c3152f6433614
nsmryan <nsmryan@gmail.com>
2016-10-15 19:59:19 -0400

    added elm to site.hs and elm directory

--- site.hs
+++ site.hs
@@ -11,6 +11,10 @@ main = hakyllWith config $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "elm/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
